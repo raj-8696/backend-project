812369b6c3995ed1468fb08762c66cf89e86e6ca
Rajnarayan <rajnarayankumar139@gmail.com>
2023-11-24 08:38:33 +0530

    some bug fiexs

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -28,10 +28,15 @@ const registerUser = asyncHandler(async (req, res, next) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
+  }
+
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path;
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
@@ -59,8 +64,8 @@ const registerUser = asyncHandler(async (req, res, next) => {
   }
 
   return res.status(201).json(
-      new ApiResponse(200, createdUser, "User registered succsuccessfully"),
-    );
+    new ApiResponse(200, createdUser, "User registered succsuccessfully"),
+  );
 });
 
 const loginUser = asyncHandler(async (req, res, next) => {
